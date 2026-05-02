9743e8aaa25574402bfe05412b3244d595f0d599
111060041 <wumhfo@gmail.com>
2026-05-03 02:34:36 +0800

    project初始化(Git/Reacr/Firebase)

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCJMOaktlLGrXfKqiee2ohn407U0Qmstko",
+    authDomain: "midterm-project-111060041.firebaseapp.com",
+    projectId: "midterm-project-111060041",
+    storageBucket: "midterm-project-111060041.firebasestorage.app",
+    messagingSenderId: "718501593024",
+    appId: "1:718501593024:web:e7149f7f694ce7fc185225"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
