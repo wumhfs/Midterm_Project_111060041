9243da5d7fb47b76af564e78b53ba51b32b7201b
111060041 <wumhfo@gmail.com>
2026-05-03 04:11:02 +0800

    更新rule

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCJMOaktlLGrXfKqiee2ohn407U0Qmstko",
-    authDomain: "midterm-project-111060041.firebaseapp.com",
-    projectId: "midterm-project-111060041",
-    storageBucket: "midterm-project-111060041.firebasestorage.app",
-    messagingSenderId: "718501593024",
-    appId: "1:718501593024:web:e7149f7f694ce7fc185225"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
